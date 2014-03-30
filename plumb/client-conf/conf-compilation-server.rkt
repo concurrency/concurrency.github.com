8c0062ae54442fcbd94a1cdcb851319686365950
Matthew Jadud <matt@jadud.com>
2014-03-30 15:42:31 -0400

    Using the Digital Ocean droplet...

--- plumb/client-conf/conf-compilation-server.rkt
+++ plumb/client-conf/conf-compilation-server.rkt
@@ -1,4 +1,10 @@
+#hash((host . "107.170.105.21")
+      (port . "80")
+      (examples . "https://raw.github.com/concurrency/plumbing-examples/master/repositories.conf")
+      )
+#|
 #hash((host . "ec2-54-226-131-120.compute-1.amazonaws.com")
       (port . "9000")
       (examples . "https://raw.github.com/concurrency/plumbing-examples/master/repositories.conf")
       )
+|#
